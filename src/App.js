de7f4d6c7ead353b14d1bfe6580d1dda11b3eca1
podeye <podeye@hotmail.com>
2020-10-05 10:31:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+// Whenever you have index files you do not need to specify .index
+// ex import {fetchData} from './api.index'
+// index is the default and will be searched from automatically
+
+import { fetchData } from './api';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    // fetch data
+    const fetchedData = await fetchData(country);
+
+    // set state
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img
+          className={styles.image}
+          src='https://i.ibb.co/7QpKsCX/image.png'
+          alt='COVID-19'
+        />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
+
+// This index.js is used to unclutter app.js with bunched imports
